b679692588313e6974daee7e3572446c09a3933a
Berkay Ozder <berkayq3535@gmail.com>
2025-05-11 21:01:43 +0300

    Bugfix

--- Scripts/SurveyData/SurveyQuestionData.cs
+++ Scripts/SurveyData/SurveyQuestionData.cs
@@ -6,11 +6,11 @@ public class SurveyQuestionData : ScriptableObject
     [TextArea(2, 4)]
     public string questionText;
 
-    [Tooltip("Google Form'daki entry ID (örnek: entry.1234567890)")]
+    [Tooltip("Entry ID in the Google Form (e.g., entry.1234567890)")]
     public string entryID;
 
     public SurveyQuestionType questionType = SurveyQuestionType.YesNo;
 
-    [Tooltip("Eğer bu soru son soruyu temsil ediyorsa, form gönderimi yapılır.")]
+    [Tooltip("If this question is marked as final, the form will be submitted.")]
     public bool isFinal = false;
 }
